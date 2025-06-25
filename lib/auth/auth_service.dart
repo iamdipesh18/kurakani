d1fe4531680d779952b81d3f36f3547f631d4106
Dipesh Dhungana <dipesh.077bct034@acem.edu.np>
2025-06-25 16:32:23 +0545

    setting up firebase and also starting the auth services codes

---
+++ lib/auth/auth_service.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  //instace of auth
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<UserCredential> signInWithEmailPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+  //sign in
+
+  //sign up
+
+  //sign out
+
+  //errors
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kurakani/auth/login_or_register.dart';
+import 'package:kurakani/firebase_options.dart';
 import 'package:kurakani/themes/light_mode.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
